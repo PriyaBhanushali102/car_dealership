6622177d475225dacfca4b53535e4645f2fffffe
Priya Bhanushali <bhanushalipriya567@gamil.com>
2026-07-22 02:02:36 +0530

    feat: initial backend setup and health check test

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import app from "./app.js";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB connected successfully");
+
+    app.listen(PORT, () => {
+      console.log(`Server running on ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
