045fd1d5009be1b335ab337b99e19e295ad8d685
Priya <bhanushalipriya567@gmail.com>
2026-07-22 15:46:51 +0530

    git commit -m "feat(auth): add user model with password hashing"

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 
 const userSchema = new mongoose.Schema(
   {
@@ -26,5 +27,17 @@ const userSchema = new mongoose.Schema(
     timestamps: true,
   },
 );
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return;
+  }
+
+  this.password = await bcrypt.hash(this.password, 10);
+});
+
+userSchema.methods.matchPassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
 
 export default mongoose.model("User", userSchema);
